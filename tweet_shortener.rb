07198ead8671d82d77b96a9cbdbefc9aa7aa99c3
Danny Wakeling <wakeling101@hotmail.com>
2019-03-07 19:43:56 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,3 +13,20 @@ def dictionary
    "and" => "&"
 }
 end
+
+def word_substituter(tweet)
+  # replaces long words with their expected short form
+    tweet.split("").map do |word|
+      if dictionary.keys.include?(word.downcase)
+        word = dictionary[word.downcase]
+      else
+        word
+    end
+  end.join("")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweet.each do |phrase|
+    puts word_substituter(phrase)
+  end
+end
